40164e21abb3847455b2750bdd859fda9cee14de
Mohan Krishna Chinta <mohankrishnachinta@Mohans-MacBook-Pro.local>
2026-01-28 12:39:14 -0500

    added the bank details

---
+++ 01_variables.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+BANK = BOFA
+ACCOUNT = Checking
+NAME = MOHAN
+
+echo "Here is the Mybank name $BANK"
+echo "Here is the my type of account $ACCOUNT"
+echo "My name is the $NAME"
+
+
+
