74fe9afa2d33477fe7fd0fe65447b0950b39f4dc
Mohan Krishna Chinta <mohankrishnachinta@Mohans-MacBook-Pro.local>
2026-01-28 13:03:09 -0500

    Added the 02_variable.sh

---
+++ 02_Arg_varibles.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+NUM1=$1 
+NUM2=$2
+ 
+echo: "What is the first number? $NUM1"
+echo: "What is the second number? $NUM2"
+
+if [ $NUM1 -ne $NUM2 ]
+then
+    echo "The numbers are not equal"
+else
+    echo "The numbers are equal"
+fi
