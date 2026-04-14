c6c5d9b554150e21c57cc6dd878c6c6064ada6ed
Mohan Krishna Chinta <mohankrishnachinta@Mohans-MacBook-Pro.local>
2026-04-13 21:03:52 -0400

    updated file

---
+++ 08_functions.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ];then
+    echo "Please run this script with root user access"
+    exit 1
+fi  
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE."
+        exit 1
+    else 
+        echo "$2 ... SUCCESS."
+    fi
+}
+
+
